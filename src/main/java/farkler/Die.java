101768f0efaaa662758102caac945fe8036b5e6e
gboone11 <gavin.boone@ejgallo.com>
2026-05-22 14:37:17 -0700

    keeps track of scores

--- src/main/java/farkler/Die.java
+++ src/main/java/farkler/Die.java
@@ -4,18 +4,26 @@ import java.util.Random;
 
 public class Die {
     private final int sides;
+    private int value;
 
     public Die(int sides) {
         this.sides = sides;
+        this.value = 0;
     }
 
     public int getSides() {
         return sides;
     }
 
+    public int getValue() {
+        return value;
+    }
+
     public int roll() {
         // roll number 1-num sides
         Random random = new Random();
-        return random.nextInt(sides) + 1;
+        int result = random.nextInt(sides) + 1;
+        this.value = result;
+        return result;
     }
 }
--- src/main/java/farkler/Farkle.java
+++ src/main/java/farkler/Farkle.java
@@ -9,23 +9,37 @@ import java.util.Arrays;
 public class Farkle {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        int score = 0;
+        int numDice = 6;
+        int numDiceToTake = -1;
         while (true) {
-            System.out.print("\nNumber of dice (0 to quit): ");
-            int numDie = scanner.nextInt();
-
-            if (numDie <= 0)
+            if (numDiceToTake == 0) {
+                System.out.printf("SCORE: %d \n", score);
                 break;
+            }
 
-            if (numDie > 6) {
+            if (numDiceToTake > 6) {
                 System.out.print("Must be less than 6.");
                 continue;
             }
 
-            Die[] dice = new Die[numDie];
+            Die[] dice = new Die[numDice];
             Die die = new Die(6); // farkle uses d6s
             Arrays.fill(dice, die);
 
             System.out.printf("%s \n", Arrays.toString(Roll.roll(dice)));
+
+            System.out.print("Which dice to take? (how many, then indexes): ");
+            numDiceToTake = scanner.nextInt();
+            for (int i = 0; i < numDiceToTake; i++) {
+                int idx = scanner.nextInt();
+                if (dice[idx].getValue() == 1)
+                    score += 100;
+                if (dice[idx].getValue() == 5)
+                    score += 50;
+            }
+            numDice -= numDiceToTake;
         }
         scanner.close();
     }
