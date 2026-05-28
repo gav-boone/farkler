755794b6c3bec538ad98440f73f6e842b4e3a5eb
gboone11 <gavin.boone@ejgallo.com>
2026-05-28 16:31:46 -0700

    moved roll and indexes to global state and wrote combo and triple handling methods

--- src/main/java/farkler/Farkle.java
+++ src/main/java/farkler/Farkle.java
@@ -10,6 +10,9 @@ import java.util.Map;
  */
 public class Farkle {
     private static int SCORE;
+    private static Integer[] roll;
+    private static Integer[] indexes;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -19,9 +22,9 @@ public class Farkle {
         while (true) {
             Die d6 = new Die(6); // farkle uses d6s
 
-            Integer[] roll = d6.roll(numDice);
+            roll = d6.roll(numDice);
 
-            // roll = new Integer[] { 1, 1, 1, 3, 3, 3 };
+            roll = new Integer[] { 1, 1, 1, 1, 3, 3 };
 
             System.out.printf("%s \n", Arrays.toString(roll));
             System.out.printf("%d \n", SCORE);
@@ -38,14 +41,22 @@ public class Farkle {
                 continue;
             }
 
-            Integer[] indexes = new Integer[numDiceToTake];
-            for (int i = 0; i < numDiceToTake; i++) {
-                System.out.print("Index to take (1-6): ");
-                indexes[i] = scanner.nextInt() - 1;
+            if (numDiceToTake == 6) {
+                indexes = new Integer[] { 0, 1, 2, 3, 4, 5 };
+            } else {
+                indexes = new Integer[numDiceToTake];
+                for (int i = 0; i < numDiceToTake; i++) {
+                    System.out.print("Index to take (1-6): ");
+                    indexes[i] = scanner.nextInt() - 1;
+                }
+            }
+
+            if (indexes.length >= 4) {
+                handleBigCombos();
             }
 
             if (indexes.length >= 3) {
-                handleCombos(roll, indexes);
+                handleTriples();
             }
 
             for (int idx : indexes) {
@@ -60,8 +71,12 @@ public class Farkle {
         scanner.close();
     }
 
-    private static void handleCombos(Integer[] roll, Integer[] indexes) {
+    private static void handleBigCombos() {
         Map<Integer, Integer> counts = new HashMap<>();
+
+        for (int val : roll) {
+            counts.merge(val, 1, Integer::sum);
+        }
 
         Map<Integer, Integer> straight = Map.of(
                 1, 1,
@@ -70,18 +85,12 @@ public class Farkle {
                 4, 1,
                 5, 1,
                 6, 1);
-
-        for (int val : roll) {
-            counts.merge(val, 1, Integer::sum);
-        }
-
-        // if 6 of a kind
         if (counts.equals(straight)) {
             SCORE += 1500;
-            for (int i = 0; i < indexes.length; i++) {
-                roll[indexes[i]] = 0;
-            }
+            roll = new Integer[] { 0, 0, 0, 0, 0, 0 };
+            return;
         }
+
         int triplets = 0;
         for (int v : counts.values()) {
             if (v == 3)
@@ -89,41 +98,33 @@ public class Farkle {
         }
         if (triplets == 2) {
             SCORE += 2500;
-            for (int i = 0; i < indexes.length; i++) {
-                roll[indexes[i]] = 0;
-            }
+            roll = new Integer[] { 0, 0, 0, 0, 0, 0 };
+            return;
         }
+
         int pairs = 0;
         for (int v : counts.values()) {
             if (v == 2)
                 pairs++;
         }
         if (pairs == 3) {
-            SCORE += 1501;
-            for (int i = 0; i < indexes.length; i++) {
-                roll[indexes[i]] = 0;
-            }
+            SCORE += 1500;
+            roll = new Integer[] { 0, 0, 0, 0, 0, 0 };
+            return;
         }
+
         if (counts.values().contains(4) && counts.values().contains(2)) {
-            SCORE += 1502;
-            for (int i = 0; i < indexes.length; i++) {
-                roll[indexes[i]] = 0;
-            }
+            SCORE += 1500;
+            roll = new Integer[] { 0, 0, 0, 0, 0, 0 };
+            return;
         }
+
         if (counts.containsValue(6)) {
             SCORE += 3000;
-            int key = 0;
-            for (Map.Entry<Integer, Integer> entry : counts.entrySet()) {
-                if (entry.getValue().equals(6)) {
-                    key = entry.getKey();
-                    break;
-                }
-            }
-            for (int i = 0; i < indexes.length; i++) {
-                if (roll[indexes[i]] == key)
-                    roll[indexes[i]] = 0;
-            }
-        } 
+            roll = new Integer[] { 0, 0, 0, 0, 0, 0 };
+            return;
+        }
+
         if (counts.containsValue(5)) {
             SCORE += 2000;
             int key = 0;
@@ -137,7 +138,9 @@ public class Farkle {
                 if (roll[indexes[i]] == key)
                     roll[indexes[i]] = 0;
             }
+            return;
         }
+
         if (counts.containsValue(4)) {
             SCORE += 1000;
             int key = 0;
@@ -151,7 +154,17 @@ public class Farkle {
                 if (roll[indexes[i]] == key)
                     roll[indexes[i]] = 0;
             }
+            return;
         }
+    }
+
+    private static void handleTriples() {
+        Map<Integer, Integer> counts = new HashMap<>();
+
+        for (int val : roll) {
+            counts.merge(val, 1, Integer::sum);
+        }
+
         if (counts.getOrDefault(1, 0) == 3) {
             SCORE += 300;
             for (int i = 0; i < indexes.length; i++) {
@@ -194,6 +207,5 @@ public class Farkle {
                     roll[indexes[i]] = 0;
             }
         }
-
     }
 }
