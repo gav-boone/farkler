c8b8b4a845be7409ecf646d62395de76ba13609d
gboone11 <gavin.boone@ejgallo.com>
2026-05-28 15:56:54 -0700

    made handles combos method

--- src/main/java/farkler/Farkle.java
+++ src/main/java/farkler/Farkle.java
@@ -9,27 +9,27 @@ import java.util.Map;
  * Hello world!
  */
 public class Farkle {
+    private static int SCORE;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int score = 0;
+        SCORE = 0;
         int numDice = 6;
         int numDiceToTake = 0;
         while (true) {
             Die d6 = new Die(6); // farkle uses d6s
 
             Integer[] roll = d6.roll(numDice);
-            
 
             // roll = new Integer[] { 1, 1, 1, 3, 3, 3 };
 
             System.out.printf("%s \n", Arrays.toString(roll));
-            System.out.printf("%d \n", score);
+            System.out.printf("%d \n", SCORE);
             System.out.print("How many dice to take (0 if farkle): ");
             numDiceToTake = scanner.nextInt();
 
             if (numDiceToTake == 0) {
-                System.out.printf("!!!!FARKLE!!!!\nSCORE: %d \n", score);
+                System.out.printf("!!!!FARKLE!!!!\nSCORE: %d \n", SCORE);
                 break;
             }
 
@@ -40,155 +40,160 @@ public class Farkle {
 
             Integer[] indexes = new Integer[numDiceToTake];
             for (int i = 0; i < numDiceToTake; i++) {
-                System.out.print("Index to take: ");
-                indexes[i] = scanner.nextInt();
+                System.out.print("Index to take (1-6): ");
+                indexes[i] = scanner.nextInt() - 1;
             }
 
             if (indexes.length >= 3) {
-                Map<Integer, Integer> counts = new HashMap<>();
-
-                Map<Integer, Integer> straight = Map.of(
-                        1, 1,
-                        2, 1,
-                        3, 1,
-                        4, 1,
-                        5, 1,
-                        6, 1);
-
-                for (int val : roll) {
-                    counts.merge(val, 1, Integer::sum);
-                }
-
-                if (counts.containsValue(6)) {
-                    score += 3000;
-                    int key = 0;
-                    for (Map.Entry<Integer, Integer> entry : counts.entrySet()) {
-                        if (entry.getValue().equals(6)) {
-                            key = entry.getKey();
-                            break;
-                        }
-                    }
-                    for (int i = 0; i < indexes.length; i++) {
-                        if (roll[indexes[i]] == key)
-                            roll[indexes[i]] = 0;
-                    }
-                }
-                if (counts.equals(straight)) {
-                    score += 1500;
-                    for (int i = 0; i < indexes.length; i++) {
-                        roll[indexes[i]] = 0;
-                    }
-                }
-                int triplets = 0;
-                for (int v : counts.values()) {
-                    if (v == 3)
-                        triplets++;
-                }
-                if (triplets == 2) {
-                    score += 2500;
-                    for (int i = 0; i < indexes.length; i++) {
-                        roll[indexes[i]] = 0;
-                    }
-                }
-                int pairs = 0;
-                for (int v : counts.values()) {
-                    if (v == 2)
-                        pairs++;
-                }
-                if (pairs == 3) {
-                    score += 1501;
-                    for (int i = 0; i < indexes.length; i++) {
-                        roll[indexes[i]] = 0;
-                    }
-                }
-                if (counts.values().contains(4) && counts.values().contains(2)) {
-                    score += 1502;
-                    for (int i = 0; i < indexes.length; i++) {
-                        roll[indexes[i]] = 0;
-                    }
-                }
-                if (counts.containsValue(5)) {
-                    score += 2000;
-                    int key = 0;
-                    for (Map.Entry<Integer, Integer> entry : counts.entrySet()) {
-                        if (entry.getValue().equals(5)) {
-                            key = entry.getKey();
-                            break;
-                        }
-                    }
-                    for (int i = 0; i < indexes.length; i++) {
-                        if (roll[indexes[i]] == key)
-                            roll[indexes[i]] = 0;
-                    }
-                }
-                if (counts.containsValue(4)) {
-                    score += 1000;
-                    int key = 0;
-                    for (Map.Entry<Integer, Integer> entry : counts.entrySet()) {
-                        if (entry.getValue().equals(4)) {
-                            key = entry.getKey();
-                            break;
-                        }
-                    }
-                    for (int i = 0; i < indexes.length; i++) {
-                        if (roll[indexes[i]] == key)
-                            roll[indexes[i]] = 0;
-                    }
-                }
-                if (counts.getOrDefault(1, 0) == 3) {
-                    score += 300;
-                    for (int i = 0; i < indexes.length; i++) {
-                        if (roll[indexes[i]] == 1)
-                            roll[indexes[i]] = 0;
-                    }
-                }
-                if (counts.getOrDefault(2, 0) == 3) {
-                    score += 200;
-                    for (int i = 0; i < indexes.length; i++) {
-                        if (roll[indexes[i]] == 2)
-                            roll[indexes[i]] = 0;
-                    }
-                }
-                if (counts.getOrDefault(3, 0) == 3) {
-                    score += 300;
-                    for (int i = 0; i < indexes.length; i++) {
-                        if (roll[indexes[i]] == 3)
-                            roll[indexes[i]] = 0;
-                    }
-                }
-                if (counts.getOrDefault(4, 0) == 3) {
-                    score += 400;
-                    for (int i = 0; i < indexes.length; i++) {
-                        if (roll[indexes[i]] == 4)
-                            roll[indexes[i]] = 0;
-                    }
-                }
-                if (counts.getOrDefault(5, 0) == 3) {
-                    score += 500;
-                    for (int i = 0; i < indexes.length; i++) {
-                        if (roll[indexes[i]] == 5)
-                            roll[indexes[i]] = 0;
-                    }
-                }
-                if (counts.getOrDefault(6, 0) == 3) {
-                    score += 600;
-                    for (int i = 0; i < indexes.length; i++) {
-                        if (roll[indexes[i]] == 6)
-                            roll[indexes[i]] = 0;
-                    }
-                }
-                
+                handleCombos(roll, indexes);
             }
 
             for (int idx : indexes) {
                 if (roll[idx] == 1)
-                    score += 100;
+                    SCORE += 100;
                 if (roll[idx] == 5)
-                    score += 50;
+                    SCORE += 50;
             }
 
             numDice -= numDiceToTake;
         }
         scanner.close();
+    }
+
+    private static void handleCombos(Integer[] roll, Integer[] indexes) {
+        Map<Integer, Integer> counts = new HashMap<>();
+
+        Map<Integer, Integer> straight = Map.of(
+                1, 1,
+                2, 1,
+                3, 1,
+                4, 1,
+                5, 1,
+                6, 1);
+
+        for (int val : roll) {
+            counts.merge(val, 1, Integer::sum);
+        }
+
+        // if 6 of a kind
+        if (counts.equals(straight)) {
+            SCORE += 1500;
+            for (int i = 0; i < indexes.length; i++) {
+                roll[indexes[i]] = 0;
+            }
+        }
+        int triplets = 0;
+        for (int v : counts.values()) {
+            if (v == 3)
+                triplets++;
+        }
+        if (triplets == 2) {
+            SCORE += 2500;
+            for (int i = 0; i < indexes.length; i++) {
+                roll[indexes[i]] = 0;
+            }
+        }
+        int pairs = 0;
+        for (int v : counts.values()) {
+            if (v == 2)
+                pairs++;
+        }
+        if (pairs == 3) {
+            SCORE += 1501;
+            for (int i = 0; i < indexes.length; i++) {
+                roll[indexes[i]] = 0;
+            }
+        }
+        if (counts.values().contains(4) && counts.values().contains(2)) {
+            SCORE += 1502;
+            for (int i = 0; i < indexes.length; i++) {
+                roll[indexes[i]] = 0;
+            }
+        }
+        if (counts.containsValue(6)) {
+            SCORE += 3000;
+            int key = 0;
+            for (Map.Entry<Integer, Integer> entry : counts.entrySet()) {
+                if (entry.getValue().equals(6)) {
+                    key = entry.getKey();
+                    break;
+                }
+            }
+            for (int i = 0; i < indexes.length; i++) {
+                if (roll[indexes[i]] == key)
+                    roll[indexes[i]] = 0;
+            }
+        } 
+        if (counts.containsValue(5)) {
+            SCORE += 2000;
+            int key = 0;
+            for (Map.Entry<Integer, Integer> entry : counts.entrySet()) {
+                if (entry.getValue().equals(5)) {
+                    key = entry.getKey();
+                    break;
+                }
+            }
+            for (int i = 0; i < indexes.length; i++) {
+                if (roll[indexes[i]] == key)
+                    roll[indexes[i]] = 0;
+            }
+        }
+        if (counts.containsValue(4)) {
+            SCORE += 1000;
+            int key = 0;
+            for (Map.Entry<Integer, Integer> entry : counts.entrySet()) {
+                if (entry.getValue().equals(4)) {
+                    key = entry.getKey();
+                    break;
+                }
+            }
+            for (int i = 0; i < indexes.length; i++) {
+                if (roll[indexes[i]] == key)
+                    roll[indexes[i]] = 0;
+            }
+        }
+        if (counts.getOrDefault(1, 0) == 3) {
+            SCORE += 300;
+            for (int i = 0; i < indexes.length; i++) {
+                if (roll[indexes[i]] == 1)
+                    roll[indexes[i]] = 0;
+            }
+        }
+        if (counts.getOrDefault(2, 0) == 3) {
+            SCORE += 200;
+            for (int i = 0; i < indexes.length; i++) {
+                if (roll[indexes[i]] == 2)
+                    roll[indexes[i]] = 0;
+            }
+        }
+        if (counts.getOrDefault(3, 0) == 3) {
+            SCORE += 300;
+            for (int i = 0; i < indexes.length; i++) {
+                if (roll[indexes[i]] == 3)
+                    roll[indexes[i]] = 0;
+            }
+        }
+        if (counts.getOrDefault(4, 0) == 3) {
+            SCORE += 400;
+            for (int i = 0; i < indexes.length; i++) {
+                if (roll[indexes[i]] == 4)
+                    roll[indexes[i]] = 0;
+            }
+        }
+        if (counts.getOrDefault(5, 0) == 3) {
+            SCORE += 500;
+            for (int i = 0; i < indexes.length; i++) {
+                if (roll[indexes[i]] == 5)
+                    roll[indexes[i]] = 0;
+            }
+        }
+        if (counts.getOrDefault(6, 0) == 3) {
+            SCORE += 600;
+            for (int i = 0; i < indexes.length; i++) {
+                if (roll[indexes[i]] == 6)
+                    roll[indexes[i]] = 0;
+            }
+        }
+
     }
 }
